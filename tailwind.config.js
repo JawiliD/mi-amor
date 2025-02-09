bc64622ba88cd5c674b158db3a2a1976a3b4196a
JawiliD <dareajawili@gmail.com>
2025-02-09 11:02:48 +0800

    greetings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 tailwind.config = {
     theme: {
         extend: {
+            textShadow: {
+                glow: '0 0 5px #fff, 0 0 10px #fff, 0 0 20px #fff, 0 0 40px #e90ed7, 0 0 80px #e90ed7, 0 0 90px #e90ed7, 0 0 100px #e90ed7, 0 0 150px #e90ed7',
+            },
             animation: {
-                blink: 'blink 1s steps(2, start) infinite',
+                heartbeat: 'heartbeat 1.5s ease-in-out infinite',
             },
             keyframes: {
-                blink: {
-                    '0%, 100%': { 'border-color': 'transparent' },
-                    '50%': { 'border-color': 'rgb(249 115 22)' }, // Tailwind's orange-500
+                heartbeat: {
+                    '0%, 100%': { transform: 'scale(1)' },
+                    '50%': { transform: 'scale(1.1)' },
                 },
+            },
+            fontFamily: {
+                comic: ["Comic Neue", "cursive"], // Add the font here
             },
         },
     },
